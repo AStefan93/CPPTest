c7317a1f97650993cddff0a2fe829073dfcfb4aa
Arnautu Stefan <arnautu.stefan93@gmail.com>
2021-08-23 17:25:40 -0400

    Implemented templated array.

--- src/array/array_test.cpp
+++ src/array/array_test.cpp
@@ -8,7 +8,7 @@
 namespace {
 TEST(Array, WhenDefaultConstructorExpectEmptyArray) {
   using ::testing::IsEmpty;
-  Array empty_array{};
+  Array<int> empty_array{};
   EXPECT_THAT(empty_array, IsEmpty());
 }
 
@@ -33,6 +33,60 @@ TEST(Array, WhenAssigningValueOutOfBoundsExpectException) {
   constexpr auto test_value{10};
   EXPECT_THROW(array_object[out_of_bounds_index] = test_value;
                , std::runtime_error);
+}
+
+TEST(Array, WhenOutputToStreamExpectNoThrows) {
+  constexpr auto size = 5;
+  Array array_object{size};
+  constexpr auto index = 1;
+  constexpr auto test_value{10};
+  array_object[index] = test_value;
+  EXPECT_NO_THROW(std::cout << array_object << std::endl);
+}
+
+TEST(Array, WhenCopyingArrayExpectCorrectValues) {
+  constexpr auto size = 5;
+  Array array_object1{size};
+  Array array_object2{size};
+  constexpr auto index = 1;
+  constexpr auto test_value1{10};
+  constexpr auto test_value2{20};
+  array_object1[index] = test_value1;
+  array_object2 = array_object1;
+  array_object1[index + 1] = test_value1;
+  array_object2[index + 1] = test_value2;
+
+  EXPECT_EQ(array_object1[index], array_object2[index]);
+  EXPECT_EQ(array_object1[index + 1], test_value1);
+  EXPECT_EQ(array_object2[index + 1], test_value2);
+}
+
+TEST(Array, WhenMoveConstructingArrayExpectCorrectValues) {
+  constexpr auto size = 5;
+  Array array_object1{size};
+  constexpr auto index = 1;
+  constexpr auto test_value1{10};
+  constexpr auto test_value2{20};
+  array_object1[index] = test_value1;
+  auto array_object2{std::move(array_object1)};
+  array_object2[index + 1] = test_value2;
+
+  EXPECT_EQ(array_object2[index], test_value1);
+  EXPECT_EQ(array_object2[index + 1], test_value2);
+}
+TEST(Array, WhenMovingByAssignmentArrayExpectCorrectValues) {
+  constexpr auto size = 5;
+  Array array_object1{size};
+  Array array_object2{size};
+  constexpr auto index = 1;
+  constexpr auto test_value1{10};
+  constexpr auto test_value2{20};
+  array_object1[index] = test_value1;
+  array_object2 = std::move(array_object1);
+  array_object2[index + 1] = test_value2;
+
+  EXPECT_EQ(array_object2[index], test_value1);
+  EXPECT_EQ(array_object2[index + 1], test_value2);
 }
 }  // namespace
 
