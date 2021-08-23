32d759ca7ead9be5a69efdd461e1456a0bbd284d
Stefan <arnautu.stefan93@gmail.com>
2021-08-23 23:42:20 +0200

    Array implementation (#18) (#19)
    * Removed main and started array implementation.
    
    * Extended Array implementation. Added address sanitizer to compiler flags.
    
    * Implemented templated array.

--- src/array/array.hpp
+++ src/array/array.hpp
@@ -1,4 +1,96 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
+#include <memory>
+
+/**
+ * @brief
+ *
+ * @tparam T
+ */
+template <typename T>
+class Array {
+ private:
+  // NOLINTNEXTLINE (cppcoreguidelines-avoid-c-arrays)
+  std::unique_ptr<T[]> m_ptr{nullptr};
+  T m_size{0};
+
+  bool IsValidIndex(T index) const { return (index >= 0) && (index < m_size); }
+
+ public:
+  Array() = default;
+  Array(const Array &source) {
+    if (!source.empty()) {
+      m_size = source.size();
+      // NOLINTNEXTLINE (cppcoreguidelines-avoid-c-arrays)
+      m_ptr = std::make_unique<T[]>(m_size);
+      for (T i = 0; i < source.size(); i++) {
+        m_ptr.get()[i] = source.m_ptr.get()[i];
+      }
+    }
+    std::cout << "COPY CONSTRUCTOR CALLED" << std::endl;
+  }
+  Array(Array &&source) {
+    if (!source.empty()) {
+      m_size = std::move(source.m_size);
+      m_ptr = std::move(source.m_ptr);
+    }
+    std::cout << "MOVE CONSTRUCTOR CALLED" << std::endl;
+  }
+  ~Array() = default;
+  explicit Array(T size) {
+    if (size != 0) {
+      // NOLINTNEXTLINE (cppcoreguidelines-avoid-c-arrays)
+      m_ptr = std::make_unique<T[]>(size);
+      m_size = size;
+    }
+    std::cout << "CONSTRUCTOR CALLED" << std::endl;
+  }
+  T size() const { return m_size; };
+  bool empty() const { return (m_size == 0); }
+  T &operator[](T index) {
+    if (!IsValidIndex(index)) {
+      throw std::runtime_error("Index out of bounds.");
+    }
+    return m_ptr.get()[index];
+  }
+  T operator[](T index) const {
+    if (!IsValidIndex(index)) {
+      throw std::runtime_error("Index out of bounds.");
+    }
+    return m_ptr.get()[index];
+  }
+  Array &operator=(const Array &source) {
+    if (!source.empty()) {
+      m_size = source.size();
+      for (T i = 0; i < source.size(); i++) {
+        m_ptr.get()[i] = source.m_ptr.get()[i];
+      }
+    }
+    std::cout << "ASSIGNMENT CALLED" << std::endl;
+
+    return *this;
+  }
+  Array &operator=(Array &&source) {
+    if (!source.empty()) {
+      m_size = source.m_size;
+      m_ptr = std::move(source.m_ptr);
+    }
+    source.m_size = 0;
+    std::cout << "MOVE ASSIGNMENT CALLED" << std::endl;
+    return *this;
+  }
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const Array<T> &a) {
+  os << "[ ";
+  for (int i = 0; i < a.size(); i++) {
+    os << a[i] << ' ';
+  }
+  os << "]";
+  return os;
+}
+
 #endif  // TEST_HPP
--- src/array/array_test.cpp
+++ src/array/array_test.cpp
@@ -1,14 +1,92 @@
 #include "array.hpp"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <stdexcept>
+
 namespace {
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(Array, WhenDefaultConstructorExpectEmptyArray) {
+  using ::testing::IsEmpty;
+  Array<int> empty_array{};
+  EXPECT_THAT(empty_array, IsEmpty());
+}
+
+TEST(Array, WhenConstructorWithSizeExpectNonEmptyArray) {
+  using ::testing::SizeIs;
+  constexpr auto size = 5;
+  Array array_object{size};
+  EXPECT_THAT(array_object, SizeIs(size));
+}
+TEST(Array, WhenAssigningValueExpectCorrectValue) {
+  constexpr auto size = 5;
+  Array array_object{size};
+  constexpr auto test_value{10};
+  array_object[0] = test_value;
+  EXPECT_EQ(array_object[0], test_value);
+}
+
+TEST(Array, WhenAssigningValueOutOfBoundsExpectException) {
+  constexpr auto size = 5;
+  Array array_object{size};
+  constexpr auto out_of_bounds_index{size + 1};
+  constexpr auto test_value{10};
+  EXPECT_THROW(array_object[out_of_bounds_index] = test_value;
+               , std::runtime_error);
+}
+
+TEST(Array, WhenOutputToStreamExpectNoThrows) {
+  constexpr auto size = 5;
+  Array array_object{size};
+  constexpr auto index = 1;
+  constexpr auto test_value{10};
+  array_object[index] = test_value;
+  EXPECT_NO_THROW(std::cout << array_object << std::endl);
+}
+
+TEST(Array, WhenCopyingArrayExpectCorrectValues) {
+  constexpr auto size = 5;
+  Array array_object1{size};
+  Array array_object2{size};
+  constexpr auto index = 1;
+  constexpr auto test_value1{10};
+  constexpr auto test_value2{20};
+  array_object1[index] = test_value1;
+  array_object2 = array_object1;
+  array_object1[index + 1] = test_value1;
+  array_object2[index + 1] = test_value2;
+
+  EXPECT_EQ(array_object1[index], array_object2[index]);
+  EXPECT_EQ(array_object1[index + 1], test_value1);
+  EXPECT_EQ(array_object2[index + 1], test_value2);
+}
+
+TEST(Array, WhenMoveConstructingArrayExpectCorrectValues) {
+  constexpr auto size = 5;
+  Array array_object1{size};
+  constexpr auto index = 1;
+  constexpr auto test_value1{10};
+  constexpr auto test_value2{20};
+  array_object1[index] = test_value1;
+  auto array_object2{std::move(array_object1)};
+  array_object2[index + 1] = test_value2;
+
+  EXPECT_EQ(array_object2[index], test_value1);
+  EXPECT_EQ(array_object2[index + 1], test_value2);
+}
+TEST(Array, WhenMovingByAssignmentArrayExpectCorrectValues) {
+  constexpr auto size = 5;
+  Array array_object1{size};
+  Array array_object2{size};
+  constexpr auto index = 1;
+  constexpr auto test_value1{10};
+  constexpr auto test_value2{20};
+  array_object1[index] = test_value1;
+  array_object2 = std::move(array_object1);
+  array_object2[index + 1] = test_value2;
+
+  EXPECT_EQ(array_object2[index], test_value1);
+  EXPECT_EQ(array_object2[index + 1], test_value2);
 }
 }  // namespace
 
