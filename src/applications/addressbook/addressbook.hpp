956081b63815db49b3e08c02ccfe21c8c8c5db08
Arnautu Stefan <arnautu.stefan93@gmail.com>
2022-04-03 16:47:57 -0400

    Cleaned up tests.

--- src/applications/addressbook/addressbook.hpp
+++ src/applications/addressbook/addressbook.hpp
@@ -2,7 +2,9 @@
 #define ADDRESSBOOK_HPP
 
 #include "external_interface/addressbook/addressbook.pb.h"
+namespace CPPTest::Addressbook {
 
 void john_doe(tutorial::Person* foo_person);
 
+}  // namespace CPPTest::Addressbook
 #endif
--- src/applications/addressbook/addressbook_test.cpp
+++ src/applications/addressbook/addressbook_test.cpp
@@ -2,34 +2,35 @@
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+namespace CPPTest::Addressbook::Test {
 
-TEST(Addressbook, WhenPersonIsSetExpectCorrectName) {
-  tutorial::Person foo_person_object;
-  tutorial::Person* foo_person = &foo_person_object;
-  john_doe(foo_person);
+class AddressbookTest : public ::testing::Test {
+  void SetUp() override { john_doe(m_foo_person); }
+  void TearDown() override {
+    m_foo_person->Clear();
+    google::protobuf::ShutdownProtobufLibrary();
+  }
 
+ protected:
+  // NOLINTNEXTLINE (cppcoreguidelines-non-private-member-variables-in-classes)
+  tutorial::Person* m_foo_person = new tutorial::Person;
+};
+
+TEST_F(AddressbookTest, WhenPersonIsSetExpectCorrectName) {
   const std::string expected_name = "John Doe";
-  const auto actual_name = foo_person->name();
+  const auto actual_name = m_foo_person->name();
   EXPECT_THAT(expected_name, actual_name);
 }
 
-TEST(Addressbook, WhenPersonIsSetExpectCorrectId) {
-  tutorial::Person foo_person_object;
-  tutorial::Person* foo_person = &foo_person_object;
-  john_doe(foo_person);
-
+TEST_F(AddressbookTest, WhenPersonIsSetExpectCorrectId) {
   constexpr auto expected_id = 1;
-  const auto actual_id = foo_person->id();
+  const auto actual_id = m_foo_person->id();
   EXPECT_THAT(expected_id, actual_id);
 }
 
-TEST(Addressbook, WhenPersonIsSetExpectCorrectEmail) {
-  tutorial::Person foo_person_object;
-  tutorial::Person* foo_person = &foo_person_object;
-  john_doe(foo_person);
-
+TEST_F(AddressbookTest, WhenPersonIsSetExpectCorrectEmail) {
   const std::string expected_email = "john.doe@test.com";
-  const auto actual_email = foo_person->email();
+  const auto actual_email = m_foo_person->email();
   EXPECT_THAT(expected_email, actual_email);
 }
 
@@ -37,3 +38,4 @@ int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+}  // namespace CPPTest::Addressbook::Test
