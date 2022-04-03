953a574ec6e988af54e9cdc6a1fb8f7de9aa4a6a
Arnautu Stefan <arnautu.stefan93@gmail.com>
2022-04-03 16:36:27 -0400

    Added main addressbook function.

--- src/applications/addressbook/addressbook.cpp
+++ src/applications/addressbook/addressbook.cpp
@@ -2,11 +2,14 @@
 
 #include "external_interface/addressbook/addressbook.pb.h"
 
-tutorial::Person addressbook_main() {
-  tutorial::Person foo_person;
-  foo_person.set_name("John Doe");
-  foo_person.set_email("john.doe@test.com");
-  foo_person.set_id(1);
+void john_doe(tutorial::Person* foo_person) {
+  foo_person->set_name("John Doe");
+  foo_person->set_email("john.doe@test.com");
+  foo_person->set_id(1);
+}
 
-  return foo_person;
+void addressbook_main() {
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+  tutorial::AddressBook address_book;
+  john_doe(address_book.add_people());
 }
