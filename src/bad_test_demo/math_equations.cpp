c78888b08bb3e454a93b268c04242be934dfef65
Stefan <arnautu.stefan93@gmail.com>
2023-03-02 08:59:04 +0000

    Add refactored methods

--- src/bad_test_demo/math_equations.cpp
+++ src/bad_test_demo/math_equations.cpp
@@ -6,18 +6,31 @@ int linear_equation(const int argument, equation_coefficients coefficients) {
   return coefficients.product * argument + coefficients.summing;
 }
 
-// int MathEquations::sum(const int argument, const int coefficient) {
+// int MathEquations::add(const int argument, const int coefficient) const {
 //   return argument + coefficient;
 // }
 
-// int MathEquations::multiply(const int argument, const int coefficient) {
+// int MathEquations::multiply(const int argument, const int coefficient) const
+// {
 //   return argument * coefficient;
+// }
+
+// int linear_equation(const std::shared_ptr<Equations>& equations,
+//                     const int argument, equation_coefficients coefficients) {
+//   const auto prd = equations->multiply(argument, coefficients.product);
+//   const auto result = equations->add(prd, coefficients.summing);
+//   return result;
 // }
 
 // int linear_equation(const int argument, equation_coefficients coefficients) {
 //   const auto prd = MathEquations::multiply(argument, coefficients.product);
 //   const auto result = MathEquations::sum(prd, coefficients.summing);
 //   return result;
+// }
+
+// int linear_equation(const std::shared_ptr<Equations>& /*unused*/,
+//                     const int argument, equation_coefficients coefficients) {
+//   return coefficients.product * argument + coefficients.summing;
 // }
 
 }  // namespace CPPTest::math_equations
--- src/bad_test_demo/math_equations_test.cpp
+++ src/bad_test_demo/math_equations_test.cpp
@@ -6,12 +6,19 @@
 
 namespace CPPTest::math_equations::Test {
 
+// class MockMathEquations : public Equations {
+//  public:
+//   MOCK_METHOD(int, add, (int argument, int coefficient), (const, override));
+//   MOCK_METHOD(int, multiply, (int argument, int coefficient),
+//               (const, override));
+// };
+
 // NOLINTNEXTLINE(cppcoreguidelines-owning-memory,cppcoreguidelines-avoid-non-const-global-variables)
 TEST(MathEquations, InputZero) {
-  const int argument = 0;
-  equation_coefficients coefficients{1, 0};
-  const int expected_result = coefficients.summing;
+  constexpr int argument = 0;
+  const equation_coefficients coefficients{0, 0};
 
+  constexpr int expected_result = 0;
   const auto actual_result = linear_equation(argument, coefficients);
 
   EXPECT_EQ(expected_result, actual_result);
@@ -19,26 +26,30 @@ TEST(MathEquations, InputZero) {
 
 // NOLINTNEXTLINE(cppcoreguidelines-owning-memory,cppcoreguidelines-avoid-non-const-global-variables)
 TEST(MathEquations, InputNotZero) {
-  const int argument = 1;
-  equation_coefficients coefficients{3, 2};
-  const int expected_result = 5;
+  constexpr int argument = 1;
+  const equation_coefficients coefficients{3, 2};
 
+  constexpr int expected_result = 5;
   const auto actual_result = linear_equation(argument, coefficients);
 
   EXPECT_EQ(expected_result, actual_result);
 }
 
-// //
 // NOLINTNEXTLINE(cppcoreguidelines-owning-memory,cppcoreguidelines-avoid-non-const-global-variables)
-// TEST(MathEquations, NotAGoodTestCase) {
+// TEST(MathEquations, NewTestCase) {
 //   const int argument = 1;
 //   equation_coefficients coefficients{3, 2};
 //   const int expected_result = 5;
 
-//   // EXPECT_CALL(sum);
-//   // EXPECT_CALL(multiply);
+//   using ::testing::_;
+//   using ::testing::Return;
+//   const auto equations = std::make_shared<MockMathEquations>();
+//   EXPECT_CALL(*equations, multiply(_, _))
+//       .WillOnce(Return(argument * coefficients.product));
+//   EXPECT_CALL(*equations, add(_, _)).WillOnce(Return(expected_result));
 
-//   const auto actual_result = linear_equation(argument, coefficients);
+//   const auto actual_result = linear_equation(equations, argument,
+//   coefficients);
 
 //   EXPECT_EQ(expected_result, actual_result);
 // }
