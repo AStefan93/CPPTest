e9dacf8f47e545edab62e36e8faf7b7df60c3625
Stefan <arnautu.stefan93@gmail.com>
2023-03-02 10:12:08 +0100

    Add refactored methods (#438)

--- src/bad_test_demo/math_equations.hpp
+++ src/bad_test_demo/math_equations.hpp
@@ -1,10 +1,17 @@
 
 namespace CPPTest::math_equations {
 
-// class MathEquations {
+// class Equations {
 //  public:
-//   static int sum(int argument, int coefficient);
-//   static int multiply(int argument, int coefficient);
+//   virtual int add(int argument, int coefficient) const = 0;
+//   virtual int multiply(int argument, int coefficient) const = 0;
+
+//   virtual ~Equations() = default;
+// };
+// class MathEquations : public Equations {
+//  public:
+//   int add(int argument, int coefficient) const override;
+//   int multiply(int argument, int coefficient) const override;
 // };
 
 struct equation_coefficients {
@@ -12,4 +19,7 @@ struct equation_coefficients {
   int product;
 };
 int linear_equation(int argument, equation_coefficients coefficients);
+// int linear_equation(const std::shared_ptr<Equations>& equations, int
+// argument,
+//                     equation_coefficients coefficients);
 }  // namespace CPPTest::math_equations
