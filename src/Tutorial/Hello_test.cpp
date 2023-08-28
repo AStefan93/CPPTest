7515755aebe1e4957a548650957d4c1027640c53
Stefan <arnautu.stefan93@gmail.com>
2023-08-29 00:19:51 +0200

    add_gtests (#738)
    * add_gtests
    
    * add conan to docker image
    
    * compile with conan
    
    * Add conan profiles for gcc
    
    * add gcc profiles
    
    * Modify CI
    
    * Fix CI

---
+++ src/Tutorial/Hello_test.cpp
@@ -0,0 +1,8 @@
+#include "Hello.hpp"
+
+#include "gtest/gtest.h"
+
+namespace CPPTest::Tutorial::Test {
+// NOLINTNEXTLINE(cppcoreguidelines-owning-memory,cppcoreguidelines-avoid-non-const-global-variables)
+TEST(Tutorial, HelloWorldTest) { FAIL(); }
+}  // namespace CPPTest::Tutorial::Test
