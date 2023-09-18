24be849a06c3120774dc79a45803c4d11327908a
Stefan <arnautu.stefan93@gmail.com>
2023-09-18 09:03:26 +0000

    AbstractFactory scheleton

---
+++ src/design_patterns/abstract_factory/abstract_factory_test.cpp
@@ -0,0 +1,9 @@
+#include "abstract_factory.hpp"
+
+#include "gtest/gtest.h"
+
+namespace CPPTest::DesignPatterns::Test {
+
+// NOLINTNEXTLINE(cppcoreguidelines-owning-memory,cppcoreguidelines-avoid-non-const-global-variables)
+TEST(AbstractFactory, initialTest) { EXPECT_TRUE(true); }
+}  // namespace CPPTest::DesignPatterns::Test
