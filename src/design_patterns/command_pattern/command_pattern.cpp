342ddf7b1863a18b710c7ba24d3dc41752705e69
Stefan <arnautu.stefan93@gmail.com>
2023-09-19 07:43:11 +0000

    Command pattern scheleton

---
+++ src/design_patterns/command_pattern/command_pattern.cpp
@@ -0,0 +1,1 @@
+#include "command_pattern.hpp"
---
+++ src/design_patterns/command_pattern/command_pattern_test.cpp
@@ -0,0 +1,10 @@
+#include "command_pattern.hpp"
+
+#include "gtest/gtest.h"
+
+namespace CPPTest::DesignPatterns::Test {
+
+// NOLINTNEXTLINE(cppcoreguidelines-owning-memory,cppcoreguidelines-avoid-non-const-global-variables)
+TEST(CommandPattern, firstTest) { EXPECT_TRUE(true); }
+
+}  // namespace CPPTest::DesignPatterns::Test
