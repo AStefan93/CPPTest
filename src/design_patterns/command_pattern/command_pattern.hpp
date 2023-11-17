899fda1d0294916f827da377c05df52144be3d5d
Stefan <arnautu.stefan93@gmail.com>
2023-11-17 16:12:02 +0000

    command_pattern (#975)
    * Add command itf
    
    * Improve debug configuration
    
    * Fix vscode complaints.
    
    * Fix CTest
    
    * CMake port for bad_test_demo
    
    * Add document mock object
    
    * Fix File interface

---
+++ src/design_patterns/command_pattern/command_pattern.hpp
@@ -0,0 +1,53 @@
+#include <memory>
+
+#include "command_pattern_itf.hpp"
+
+namespace CPPTest::DesignPatterns {
+
+class File {
+ public:
+  virtual ~File() = default;
+  File(const File&) = default;
+  File& operator=(const File&) = default;
+  File(File&&) = delete;
+  File& operator=(File&&) = delete;
+
+  virtual void paste() = 0;
+  virtual void cut() = 0;
+  virtual void copy() = 0;
+
+ protected:
+  File() = default;
+};
+
+class Paste : public Command {
+ public:
+  Paste() = default;
+  ~Paste() override = default;
+  Paste(const Paste&) = default;
+  Paste& operator=(const Paste&) = default;
+  Paste(Paste&&) = delete;
+  Paste& operator=(Paste&&) = delete;
+
+  explicit Paste(std::shared_ptr<File> file);
+
+  void execute() override;
+
+ private:
+  std::shared_ptr<File> m_document;
+};
+
+class Document : public File {
+ public:
+  Document() = default;
+  ~Document() override = default;
+  Document(const Document&) = default;
+  Document& operator=(const Document&) = default;
+  Document(Document&&) = delete;
+  Document& operator=(Document&&) = delete;
+
+  void paste() override;
+  void cut() override;
+  void copy() override;
+};
+}  // namespace CPPTest::DesignPatterns
--- src/design_patterns/command_pattern/command_pattern_test.cpp
+++ src/design_patterns/command_pattern/command_pattern_test.cpp
@@ -1,10 +1,24 @@
 #include "command_pattern.hpp"
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 namespace CPPTest::DesignPatterns::Test {
 
+class MockDocument : public File {
+ public:
+  MOCK_METHOD(void, paste, (), (override));
+  MOCK_METHOD(void, cut, (), (override));
+  MOCK_METHOD(void, copy, (), (override));
+};
+
 // NOLINTNEXTLINE(cppcoreguidelines-owning-memory,cppcoreguidelines-avoid-non-const-global-variables)
-TEST(CommandPattern, firstTest) { EXPECT_TRUE(true); }
+TEST(CommandPattern, paste) {
+  const auto document = std::make_shared<MockDocument>();
+  Paste cut(document);
+
+  EXPECT_CALL(*document, paste()).Times(1);
+  cut.execute();
+}
 
 }  // namespace CPPTest::DesignPatterns::Test
