2c20d4191d807f6193458130b77c606ad8668a59
Stefan <arnautu.stefan93@gmail.com>
2023-12-15 08:51:47 +0000

    Add proxy_pattern project

---
+++ src/design_patterns/proxy_pattern/proxy_pattern_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+namespace CPPTest::DesignPatterns::Test {
+
+// NOLINTNEXTLINE(cppcoreguidelines-owning-memory,cppcoreguidelines-avoid-non-const-global-variables)
+TEST(ProxyPattern, dummy) { EXPECT_TRUE(true); }
+
+}  // namespace CPPTest::DesignPatterns::Test
