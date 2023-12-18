1f31443f35291c1034667227822ed9a497c74324
Stefan <arnautu.stefan93@gmail.com>
2023-12-18 10:12:19 +0000

    Add state pattern skeleton

--- src/design_patterns/proxy_pattern/proxy_pattern_test.cpp
+++ src/design_patterns/proxy_pattern/proxy_pattern_test.cpp
@@ -8,13 +8,13 @@
 
 namespace CPPTest::DesignPatterns::Test {
 
-const std::string DATA_FILES_PATH = "/workspace/CPPTest/test_files/";
+// const std::string DATA_FILES_PATH = "/workspace/CPPTest/test_files/";
 
 class AProxyPattern : public ::testing::Test {
 public:
   AProxyPattern() {
-    m_graphics = std::make_shared<Graphics>(
-        std::make_shared<ImageProxy>(DATA_FILES_PATH + "image_0.txt"));
+    m_graphics =
+        std::make_shared<Graphics>(std::make_shared<ImageProxy>("image_0.txt"));
   }
 
 protected:
@@ -22,25 +22,25 @@ protected:
   std::shared_ptr<GraphicsItf> m_graphics;
 };
 
-TEST_F(AProxyPattern, DISABLED_WithAProxyLoadsImageOnlyWhenDrawn) {
+TEST_F(AProxyPattern, WithAProxyLoadsImageOnlyWhenDrawn) {
   EXPECT_NO_THROW(m_graphics->draw());
 }
-TEST_F(AProxyPattern, DISABLED_WithMultipleProxiesDoesNotLoadFiles) {
+TEST_F(AProxyPattern, WithMultipleProxiesDoesNotLoadFiles) {
 
   constexpr auto number_of_files = 20'000;
   for (auto i = 0; i < number_of_files; ++i) {
     const std::shared_ptr<ImageItf> image_proxy =
-        std::make_shared<ImageProxy>(DATA_FILES_PATH + "image_0.txt");
+        std::make_shared<ImageProxy>("image_0.txt");
     EXPECT_NO_THROW(m_graphics->add_file(image_proxy));
   }
 }
 
-TEST_F(AProxyPattern, DISABLED_WithMultipleProxiesLoadsAllFilesWhenDrawn) {
+TEST_F(AProxyPattern, WithMultipleProxiesLoadsAllFilesWhenDrawn) {
 
   constexpr auto number_of_files = 20'000;
   for (auto i = 0; i < number_of_files; ++i) {
     const std::shared_ptr<ImageItf> image_proxy =
-        std::make_shared<ImageProxy>(DATA_FILES_PATH + "image_0.txt");
+        std::make_shared<ImageProxy>("image_0.txt");
     EXPECT_NO_THROW(m_graphics->add_file(image_proxy));
   }
   EXPECT_NO_THROW(m_graphics->draw());
@@ -48,16 +48,14 @@ TEST_F(AProxyPattern, DISABLED_WithMultipleProxiesLoadsAllFilesWhenDrawn) {
 
 class AnImage : public ::testing::Test {
 public:
-  AnImage() {
-    m_image = std::make_shared<Image>(DATA_FILES_PATH + "image_0.txt");
-  }
+  AnImage() { m_image = std::make_shared<Image>("image_0.txt"); }
 
 protected:
   // NOLINTNEXTLINE: (misc-non-private-member-variables-in-classes)
   std::shared_ptr<Image> m_image;
 };
 
-TEST_F(AnImage, DISABLED_LoadsImageOnlyWhenDrawn) {
+TEST_F(AnImage, LoadsImageOnlyWhenDrawn) {
   EXPECT_NO_THROW(m_image->draw("Dummy text."));
 }
 
---
+++ src/design_patterns/state_pattern/state_pattern_test.cpp
@@ -0,0 +1,8 @@
+#include "state_pattern.hpp"
+#include "gtest/gtest.h"
+
+namespace CPPTest::DesignPatterns::Test {
+
+TEST(AStatePattern, dummy) { EXPECT_TRUE(true); }
+
+} // namespace CPPTest::DesignPatterns::Test
