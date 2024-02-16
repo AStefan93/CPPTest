e4ddfe96aef1e435f1dad6531301eef5e24abf01
Stefan <arnautu.stefan93@gmail.com>
2024-02-16 16:35:04 +0000

    Strategy pattern

--- src/design_patterns/strategy_pattern/strategy_pattern_test.cpp
+++ src/design_patterns/strategy_pattern/strategy_pattern_test.cpp
@@ -1,4 +1,6 @@
+#include "strategy_pattern.hpp"
 #include "gtest/gtest.h"
+#include <memory>
 
 namespace CPPTest::DesignPatterns::Test {
 
@@ -7,6 +9,20 @@ protected:
   //   AStrategyPattern m_cut;
 };
 
-TEST_F(AStrategyPattern, dummy) { EXPECT_TRUE(true); }
+TEST_F(AStrategyPattern, WithCreditCard) {
+  PaymentService payment_service;
+  std::shared_ptr<PaymentStrategy> credit_card =
+      std::make_shared<CreditCardStrategy>();
+  payment_service.set_payment_method(credit_card);
+  EXPECT_EQ(payment_service.process_payment(), "CREDIT_CARD");
+}
+
+TEST_F(AStrategyPattern, WithBankTransfer) {
+  PaymentService payment_service;
+  std::shared_ptr<PaymentStrategy> bank_transfer =
+      std::make_shared<BankTransferStrategy>();
+  payment_service.set_payment_method(bank_transfer);
+  EXPECT_EQ(payment_service.process_payment(), "BANK_TRANSFER");
+}
 
 } // namespace CPPTest::DesignPatterns::Test
